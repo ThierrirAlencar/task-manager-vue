0f8df14bf79813ffde15c27c50faaf6dfb2aa44f
ciringa <thierrird957468@gmail.com>
2026-02-06 16:59:20 -0300

    [#}refactor: Rebased everything

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<template>
+    <header class="w-full bg-slate-100 border-b-slate-200 border-b-2 p-5 flex flex-row items-center justify-center">
+    <div class="p-2 bg-green-500 w-16 h-16 flex items-center justify-center rounded-sm ">
+      <i class="pi pi-calendar-clock text-white " style="font-size: 2rem;"></i>
+    </div>
+    <div class="flex flex-col justify-center items-start w-1/4 bg-sky-50 p-2 ">
+      <h3 class="text-black font-bold text-2xl ">Rotina+</h3>
+      <p>Seu principal app de tarefas!</p>
+    </div>
+    <div class="bg-sky-50 w-1/4 h-auto flex justify-end p-2 items-center gap-0.5">
+      <div class="bg-green-500 p-2 rounded-sm"> 
+          <li class="pi pi-user text-white cursor-pointer " style="font-size: 2rem; "></li>
+      </div>
+      <div class="bg-green-500 p-2 rounded-sm"> 
+          <li class="pi pi-cog text-white cursor-pointer " style="font-size: 2rem; "></li>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export default {
+        props:{
+            title:String,
+            completed:Boolean,
+            description:String || null,
+        }
+    }
+</script>
+
+<template>
+    <div :title="description" :class="['border', 'w-full', 'border-gray-300', 'h-auto', 'p-3', 'flex', 'flex-row', 'justify-start', 'items-center', 'gap-3', 'rounded-2xl', completed ? 'bg-green-300' : 'bg-sky-50']">
+        <input type="checkbox" name="" id="" class="w-1/12" v-on:click="()=>{}">
+        <h1 class="w-10/12">{{ title }}</h1>
+        <button class="pi pi-trash w-1/12 text-red-500 text-xm cursor-pointer"></button>
+    </div>
+</template>
+
+<style scoped>
+  input[type="checkbox"] {
+    padding: 6px;
+    transform: scale(1.3);
+    accent-color: oklch(72.3% 0.219 149.579); /* Change to desired color */
+  }
+</style>
